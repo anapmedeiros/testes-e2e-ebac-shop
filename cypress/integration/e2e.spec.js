7bb54e52099d4e6e58aa67218d3850de8ea706fa
Ana Paula Medeiros <anapmedeiros@gmail.com>
2022-03-21 12:08:40 -0300

    alterações 21/03

--- cypress/integration/e2e.spec.js
+++ cypress/integration/e2e.spec.js
@@ -10,18 +10,14 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
         Adicionando ao carrinho
         Preenchendo todas opções no checkout
         E validando minha compra ao final */
-
     before(() => {
         cy.visit('minha conta/')
-
     });
-
-    it('Deve acessar a loja EBAC SHOP e fazer login', () => {
+    it('Deve acessar a loja EBAC SHOP e fazer login e concluir compra', () => {
         cy.fixture('perfil').then((dados) => {
             cy.login(dados.usuario, dados.senha)
         })
         cy.get('.page-title').should('contain', 'Minha conta')
-
         it('Deve acessar a loja e selecionar quantidade')
         cy.get('#primary-menu > .menu-item-536 > .dropdown-toggle').click()
         cy.addProdutos('Ariel Roll Sleeve Sweatshirt', 'XL', 'Red', '4')
@@ -29,20 +25,12 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
         cy.get('.woocommerce-message > .button').click()
         cy.get('.checkout-button').click()
 
-
-    });
-
-    it('Deve preencher o campo de endereço com sucesso e concluir compra', () => {
         EnderecoPage.editarEndereco('Ana', 'Medeiros', 'Brasil', 'Rua pernambucana', '200', 'Osasco', 'São Paulo', '01254-963', '11965478965', 'anapmede@gmail.com')
-
     
-    })
-
-    it('Deve concluir compra com sucesso', () => {
         cy.get('#payment_method_cod').click()
         cy.get('#terms').click()
         cy.get('#place_order').click()
         cy.get('.page-title').should('contain', 'Pedido recebido')
+    
     });
-
 })
--- cypress/integration/endereco.spec.js
+++ cypress/integration/endereco.spec.js
@@ -1,5 +1,17 @@
 /// <reference types="cypress" />
 import EnderecoPage from '../support/page_objects/nome-funcionliada.page'
+const perfil = require('../fixtures/perfil.json')
+
+beforeEach(() => {
+    cy.visit('minha-conta')
+});
+
+it('Deve colocar 4 prutos no carrinho', () => {
+    cy.get('#primary-menu > .menu-item-536 > .dropdown-toggle').click()
+cy.addProdutos('Ariel Roll Sleeve Sweatshirt', 'XL', 'Red', '4')
+cy.get('.woocommerce-message').should('contain', '4 × “Ariel Roll Sleeve Sweatshirt” foram adicionados no seu carrinho.')
+cy.get('.woocommerce-message > .button').click()
+cy.get('.checkout-button').click()
 
 describe('Funcionalidade endereço', () => {
     beforeEach(() => {
@@ -18,3 +30,5 @@ describe('Funcionalidade endereço', () => {
 
     
 });
+
+})
